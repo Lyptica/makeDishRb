71fff466730f0bc3515681bda234ff900afc4ba6
Lyptica <LypticAzure@cindySweet.local>
2017-04-22 16:56:26 +0900

    add makeDish

--- makeDish.rb
+++ makeDish.rb
@@ -9,22 +9,20 @@ class MakeDish
   # nameは一意である前提。
   @@recipes = []
   def initialize()
-    # そもそも何するプログラムか？
-    # -> 食材とレシピを入れて、作れるかどうかを確認するプログラム
-    # generateSampleItems: サンプル食材を作って配列にupdate
-    # showItems: 食材の一覧をls
-    # generateSampleRecipes: サンプルレシピをupdate
-    # showRecipes: 作ったレシピの数と内容をls
-    # makeRecipe: レシピ名を指定して作れるかどうか確認
+    # そもそも何するプログラムか -> 食材とレシピを入れて、作れるかどうかを確認するプログラム
+    # [OK] generateSampleItems: サンプル食材を作って配列にupdate
+    # [OK]showItems: 食材の一覧をls
+    # [OK]generateSampleRecipes: サンプルレシピをupdate
+    # [OK]showRecipes: 作ったレシピの数と内容をls
+    # [OK]makeRecipe: レシピ名を指定して作れるかどうか確認
 
     generateSampleItems()
     generateSampleRecipes()
 
-    # pp @@items
-    # pp @@recipes
-
     showItems()
     showRecipes()
+    makeRecipe("トマトキューリ")
+    print ("\n")
   end
 
   def generateItem(name, description, amount)
@@ -32,11 +30,12 @@ class MakeDish
   end
 
   def generateSampleItems()
-    generateItem("トマト", "赤くておいしいトマト。リコピンたっぷり！" , 1)
+    generateItem("トマト", "赤くておいしいトマト。リコピンたっぷり！" , 5)
     generateItem("きゅうり", "サクサク食感が癖になるきゅうり。あまり栄養は無いらしい。" , 1)
   end
 
   def showItems()
+    print "\n====== 冷蔵庫の中身を覗きます ======\n"
     @@items.each_with_index do |v,i|
       print "------ item:#{format('%02d', i+1)} ------\n"
       print "name: #{v[0][0]}\n"
@@ -55,11 +54,12 @@ class MakeDish
   def generateSampleRecipes()
     items = []
     items.push ["トマト", 3]
-    items.push ["キューリ", 2]
+    items.push ["きゅうり", 2]
     generateRecipe("トマトキューリ", "トマトとキューリの絶望的なコンビネーション！", items)
   end
 
   def showRecipes()
+    print "\n====== レシピ本を読みます ======\n"
     @@recipes.each_with_index do |v,i|
       print "------ recipe:#{format('%02d', i+1)} ------\n"
       print "name: #{v[0][0]}\n"
@@ -69,6 +69,56 @@ class MakeDish
         print "> name: #{vv[0]}, amount: #{vv[1]}\n"
       end
     end
+  end
+
+  def makeRecipe(recipename)
+    print "\n====== これから調理を始めます ======\n"
+    isValidRecipe = false
+    presentRecipe = nil
+    @@recipes.each do |v|
+      if recipename == v[0][0] then
+        isValidRecipe = true
+        presentRecipe = v[1]
+      end
+    end
+    if isValidRecipe == true
+      print "お望みのレシピが見つかりました: #{recipename}\n"
+      canCook = true
+      print "調理に必要な数を計算します...\n"
+      presentRecipe.each do |v|
+        # 持ってる数 >= レシピの数
+        if showItemAmount(v[0]) >= v[1] then
+          print "> #{v[0]}の数は十分です。\n"
+        else
+          canCook = false
+          print "> #{v[0]}の数が足りません。あと#{v[1]-showItemAmount(v[0])}個用意してください。\n"
+        end
+      end
+      if canCook == true
+        print "🙆  #{recipename} は作れるようです。\n"
+      else
+        print "😨  #{recipename} は作れませんでした。\n"
+      end
+    else
+      print "レシピ名が存在しません。\n"
+    end
+  end
+
+  def showItemAmount(itemname)
+    idx = findItemIndex(itemname)
+    amount = @@items[idx][1]
+    amount = -1 if amount == nil
+    return amount
+  end
+
+  def findItemIndex(itemname)
+    idx = -1
+    @@items.each_with_index do |v,i|
+      if v[0][0] == itemname then
+        idx = i
+      end
+    end
+    return idx
   end
 
 end
